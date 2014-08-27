e64ccbc51420150a2863ff973967574216bbd4e9
N0Paranoia <martijn.manneveld@gmail.com>
2014-08-27 16:23:05 +0200

    v0.0.58
    Some ai stuff

--- ai.py
+++ ai.py
@@ -5,13 +5,24 @@ from constants import *
 class Ai(object):
 
 	def __init__(self):
-		self.velocity_x = 4
+		self.velocity_x = 0
 		self.velocity_y = 0
+		self.speed = 4
+		self.LEFT = True
+		self.RIGHT = False
 
 	def move(self, x, y):
-		x -= self.velocity_x
+
+
 		if x <= 0:
-			x += self.velocity_x
-		elif x >= MAPWIDTH*32:
-			x -= self.velocity_x
+		 	self.velocity_x = self.speed
+		elif x >= MAPWIDTH*32 - TILESIZE:
+		 	self.velocity_x = -self.speed
+		else:
+			self.velocity_x = -self.speed
+
+		x += self.velocity_x
+
+	 	print self.velocity_x
+
 		return x
