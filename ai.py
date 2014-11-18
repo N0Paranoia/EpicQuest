d60a7a6afbcbeb37926a3795c26f71cebb02b8b1
N0Paranoia <martijn.manneveld@gmail.com>
2014-11-18 16:19:09 +0100

    v0.0.117
    AI

--- ai.py
+++ ai.py
@@ -8,10 +8,11 @@ class Ai(object):
 	def __init__(self):
 		self.velocity_x = 0
 		self.velocity_y = 0
-		self.speed = 4
-		self.LEFT = True
-		self.RIGHT = False
-		self.BACK = False
+		self.LEFT = False
+		self.RIGHT = True
+		self.LEFTx = [0,1]
+		self.RIGHTx = [0,1]
+
 
 	def falling(self, gravity, x, y, width, height):
 		self.is_falling = True
@@ -35,13 +36,15 @@ class Ai(object):
 		if x < 0 or x > MAPWIDTH*32 - TILESIZE or Collision().TileCollision(x, y, width, height, x+TILESIZE, y+TILESIZE, WALL) or Collision().TileCollision(x, y+TILESIZE, width, height, x+TILESIZE, y+TILESIZE, LAVA) or Collision().TileCollision(x, y+TILESIZE, width, height, x+TILESIZE, y+TILESIZE, SKY) or Collision().TileCollision(x, y, width, height, x+TILESIZE, y+TILESIZE, SLOPE_LEFT) or Collision().TileCollision(x, y, width, height, x+TILESIZE, y+TILESIZE, SLOPE_RIGHT) or Collision().TileCollision(x, y+TILESIZE, width, height, x+TILESIZE, y+TILESIZE, SLOPE_LEFT) or Collision().TileCollision(x, y+TILESIZE, width, height, x+TILESIZE, y+TILESIZE, SLOPE_RIGHT):
 			x -= self.velocity_x
 
-			self.LEFT = not self.LEFT
-			self.RIGHT = not self.RIGHT
+			self.velocity_x = -speed
+			# self.LEFT = not self.LEFT
+			# self.RIGHT = not self.RIGHT
 
+			print "xVel", self.velocity_x, numberOfMobs
 		return x
 
 		y += self.velocity_y
 		if y < 0 or y > MAPHEIGHT*32 - TILESIZE or Collision().TileCollision(x, y, width, height, x+TILESIZE, y+TILESIZE, WALL) == True:
 			y -= self.velocity_y
 
-	 	return y
+		return y
--- mobs.py
+++ mobs.py
@@ -25,28 +25,11 @@ class Mobs(object):
 	def movement(self):
 		for i in range (self.mobNumber):
 			fall = ai.falling(GRAVITY, self.xX[i], self.yY[i], self.width, self.height)
-			move = ai.move(self.xX[i], self.yY[i], self.width, self.height, self.speed1, self.mobNumber)
+			move = ai.move(self.xX[i], self.yY[i], self.width, self.height, self.speed1, i)
 			self.xX[i] = move
 			self.yY[i] = fall
-
-		# fall = ai.falling(GRAVITY, self.x, self.y, self.width, self.height)
-		fall1 = ai.falling(GRAVITY, self.x1, self.y1, self.width, self.height)
-		fall2 = ai.falling(GRAVITY, self.x2, self.y2, self.width, self.height)
-		# move = ai.move(self.x, self.y, self.width, self.height, self.speed1)
-		move1 = ai.move(self.x1, self.y1, self.width, self.height, self.speed1, self.mobNumber)
-		move2 = ai.move(self.x2, self.y2, self.width, self.height, self.speed2, self.mobNumber)
-		# self.y = fall
-		# self.x = move
-		self.y1 = fall1
-		self.x1 = move1
-		self.y2 = fall2
-		self.x2 = move2
-
-
+			
 	def render(self, window, camX, camY):
-		# pygame.draw.rect(window, YELLOW, (self.x - camX, self.y - camY, self.width, self.height))
-		pygame.draw.rect(window, YELLOW, (self.x1 - camX, self.y1 - camY, self.width, self.height))
-		pygame.draw.rect(window, GREEN, (self.x2 - camX, self.y2 - camY, self.width, self.height))
 		for i in range (self.mobNumber):
 			pygame.draw.rect(window, RED, (self.xX[i] - camX, self.yY[i] - camY, self.width, self.height))
 
