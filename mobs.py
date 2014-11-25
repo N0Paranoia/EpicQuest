b68f8db62c09349968a1192dda025687c79066ae
N0Paranoia <martijn.manneveld@gmail.com>
2014-11-25 15:13:46 +0100

    v0.0.126
    MobsMap to generate mobs

--- mobs.py
+++ mobs.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 from constants import *
+from level import *
 from ai import *
 
 ai = Ai()
@@ -16,6 +17,18 @@ class Mobs(object):
 		self.speed2 = 2
 		self.mobNumber = 2
 
+	def initializeMobs(self, x, y):
+		self.x1 = x
+		self.y1 = y
+
+	def generateMob(self, window, camX, camY):
+		for row in range(MAPHEIGHT):
+			for column in range (MAPWIDTH):
+				if column * TILESIZE > camX - TILESIZE and column * TILESIZE < camX + WINDOW_WIDTH and row * TILESIZE > camY - TILESIZE and row * TILESIZE < camY + WINDOW_HEIGHT:
+					if mobsMap[row][column] == 5:
+						self.initializeMobs(column * TILESIZE, row * TILESIZE)
+						self.renderMobs(window, camX, camY)
+
 	def movement(self, mobs, playerX, playerY):
 		fall = ai.falling(GRAVITY, self.x[mobs], self.y[mobs], self.width, self.height)
 		move = ai.move(self.x[mobs], self.y[mobs], self.width, self.height, self.speed1, mobs, playerX, playerY)
@@ -25,7 +38,11 @@ class Mobs(object):
 	def render(self, window, camX, camY, mobs):
 		pygame.draw.rect(window, RED, (self.x[mobs] - camX, self.y[mobs] - camY, self.width, self.height))
 
+	def renderMobs(self, window, camX, camY):
+		pygame.draw.rect(window, YELLOW, (self.x1 - camX, self.y1 - camY, self.width, self.height))
+
 	def update(self, window, camX, camY, playerX, playerY):
+		self.generateMob(window, camX, camY)
 		for mobs in range (self.mobNumber):
 			if (self.x[mobs] > camX and self.y[mobs] > camY and self.x[mobs] < camX + WINDOW_WIDTH and self.y[mobs] < camY + WINDOW_HEIGHT):
 				self.movement(mobs, playerX, playerY)
